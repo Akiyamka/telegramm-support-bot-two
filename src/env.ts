1c67d0a25741149ace99953ade4dc417d60830fe
Alexandr Dubinin <akiyamka@gmail.com>
2021-12-26 15:07:51 +0100

    add missing env vars

--- src/env.ts
+++ src/env.ts
@@ -3,7 +3,9 @@ import { Env } from './types';
 const env: Env = {
   BOT_AUTH_TOKEN: process.env.BOT_AUTH_TOKEN as string,
   GREETING_MESSAGE: process.env.GREETING_MESSAGE as string,
-  TELEGRAM_SUPPORT_CHAT_ID: process.env.TELEGRAM_SUPPORT_CHAT_ID as string
+  TELEGRAM_SUPPORT_CHAT_ID: process.env.TELEGRAM_SUPPORT_CHAT_ID as string,
+  DOMAIN: process.env.DOMAIN as string,
+  PORT: process.env.PORT as string, // heroku setup it
 }
 
 export default env;
--- src/types.ts
+++ src/types.ts
@@ -2,4 +2,6 @@ export interface Env {
   BOT_AUTH_TOKEN: string;
   GREETING_MESSAGE: string;
   TELEGRAM_SUPPORT_CHAT_ID: string;
+  DOMAIN: string;
+  PORT: string;
 }
